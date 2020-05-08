62aa646cd860364c512818c1f3215451e048d524
Timothy <tim87henry@gmail.com>
2020-05-08 21:26:15 +1000

    Added basic calculation capability

--- calc.js
+++ calc.js
@@ -1,7 +1,34 @@
 const number=document.getElementById('numValue');
-document.querySelector('html').style.backgroundColor="pink";
-
-
+let numberOne;
+let task;
 function setNumber(num) {
-    number.value=num;
+    if(num==='') {
+        number.value='';
+    } else {
+        if (number.value==='0') {
+            number.value=num;
+        } else {
+            number.value=number.value+num;
+        }
+    }
+}
+
+function operate(opt) {
+    if (opt==='resolve') {
+        switch (task) {
+            case 'add':
+                number.value=numberOne+Number(number.value);
+                break;
+            case 'sub':
+                number.value=numberOne-Number(number.value);
+                break;
+            default:
+                number.value=numberOne+Number(number.value);
+        }
+    } else {
+        task=opt;
+        numberOne= Number(number.value);
+        number.value='';
+    }
+    
 }
